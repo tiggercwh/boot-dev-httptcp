6fb66a70674e173a71591c454d564d7ab8c6a01b
linux-tigger <tiggercwh@icloud.com>
2025-03-31 10:34:43 +0900

    ch5 l1 original sol

---
+++ internal/headers/headers_test.go
@@ -0,0 +1,46 @@
+package headers
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHeadersParse(t *testing.T) {
+	// Test: Valid single header
+	headers := NewHeaders()
+	data := []byte("Host: localhost:42069\r\n\r\n")
+	n, done, err := headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, 23, n)
+	assert.False(t, done)
+
+	// Test: Valid single header with extra whitespace
+	headers = NewHeaders()
+	data = []byte("       Host: localhost:42069       \r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, 37, n)
+	assert.False(t, done)
+
+	// Test: Valid Done
+	headers = NewHeaders()
+	data = []byte("\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	assert.Equal(t, 0, n)
+	assert.True(t, done)
+
+	// Test: Invalid spacing header
+	headers = NewHeaders()
+	data = []byte("       Host : localhost:42069       \r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+}
---
+++ internal/headers/main.go
@@ -0,0 +1,51 @@
+package headers
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+)
+
+const crlf = "\r\n"
+
+type Headers map[string]string
+
+func NewHeaders() Headers {
+	return make(Headers)
+}
+
+func (h Headers) Parse(data []byte) (n int, done bool, err error) {
+	idx := bytes.Index(data, []byte(crlf))
+	if idx == -1 {
+		return 0, false, nil
+	}
+	if idx == 0 {
+		return 0, true, nil
+	}
+	header := string(data[:idx])
+	trimmed := strings.TrimSpace(header) //Host: localhost:42069, Host:localhost:42069
+	splitted := strings.Split(trimmed, " ")
+	first_split := splitted[0]
+	colon_idx := strings.Index(first_split, ":")
+	if len(splitted) > 2 {
+		return 0, false, fmt.Errorf("invalid header")
+	}
+	if len(splitted) == 1 {
+		if colon_idx != len(first_split)-1 && first_split[colon_idx+1] == ':' {
+			return 0, false, fmt.Errorf("invalid header")
+		}
+		k := first_split[:colon_idx]
+		v := first_split[colon_idx+1:]
+		h[k] = v
+	}
+	if len(splitted) == 2 {
+		if colon_idx != len(first_split)-1 {
+			return 0, false, fmt.Errorf("invalid header")
+		}
+		k := first_split[:colon_idx]
+		v := splitted[1]
+		h[k] = v
+	}
+	fmt.Println(idx)
+	return idx + 2, false, nil
+}
